cc7919f65f0452b1de992325824bd606392cbdef
SC\ethan.woodruff <ethan.woodruff@my.simpson.edu>
2017-10-31 10:01:49 -0500

    edits

--- public_html/signup.php
+++ public_html/signup.php
@@ -5,21 +5,21 @@
 <title>Mama G's Cookies</title>
 </head>
 <body>
-<p>1</p>
+
 <?php include '../resources/navigation.php'; ?>
-<p>2</p>
+
 <header><h1>Mama G's Cookies</h1></header>
-<p>3</p>
+
 <img src="images/mamaglogo.jpg" alt="Mama G"/>
-<p>4</p>
+
 
 <p>Sign Up and get our newsletter and cookie recommendations</p>
-<p>5</p>
+
 
 <br/>
-<p>6</p>
+
 <?php include '../resources/footer.php'; ?>
-<p>7</p>
+
 </body>
 
 </html>
